0254d750158f4988bdd6e0e11d9189b7bba81fc9
ashitha-hub <ashithaacharya854@gmail.com>
2026-05-16 11:58:16 +0530

    initial commit

--- Project1/tms/admin/includes/footer.php
+++
@@ -1,3 +0,0 @@
-<div class="copyrights">
-	 <p>TMS. All Rights Reserved |  <a href="#">TMS</a> </p>
-</div>	
--- script.js
+++
@@ -1,5 +0,0 @@
-const mammoth = require('mammoth');
-const fs = require('fs');
-mammoth.extractRawText({path: 'Ashitha H M.docx'}).then(function(result){
-    fs.writeFileSync('resume_text.txt', result.value);
-}).done();
